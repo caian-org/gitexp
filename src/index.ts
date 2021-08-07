0688aec9b723fe342d2108fe88fa7b38a133f1b8
Caian R. Ertl <hi@caian.org>
2021-08-07 09:54:09 -0300

    fix: remove example code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
 import 'module-alias/register'
-import Calculator from '@yatt/Calculator'
-import Logger from '@yatt/lib/Logger'
+import Logger from '@ge/lib/Logger'
 
-const logger = new Logger({ isDev: true, label: 'yatt' })
+const logger = new Logger({ isDev: true, label: 'ge' })
 
-/**
- * Repeats a given callback function N times.
- *
- * @param n - the amount of times to repeat.
- * @param cb - the callback function.
- */
-const each = (n: number, cb: (v: any, i: number) => void): void =>
-  Array.from(Array(n).keys()).forEach(cb)
-
-/**
- * Calculate and outputs a multiplication table of a given value.
- *
- * @param operand - the multiplication number.
- */
-function multiplicationTable (operand: number): void {
-  logger.warn('-'.repeat(30))
-  logger.warn(`Times table of ${operand}:`)
-
-  each(10, (_, i) => {
-    const iter = i + 1
-    const result = new Calculator(iter).times(operand).equals()
-
-    logger.info(`${iter} x ${operand} = ${result}`)
-  })
-}
-
-each(10, (_, i) => multiplicationTable(i + 1))
+logger.info('hello world')
